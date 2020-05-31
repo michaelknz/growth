401fc1936486a750cccef8c9b09c35d9a19ae7b4
michaelknz <mihail2003rar@mail.ru>
2020-05-31 22:09:58 +0300

    initial commit

---
+++ Assets/Prefubs/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
