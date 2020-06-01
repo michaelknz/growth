df994c420ea68c05aa863c7b3fa119928f3e5d89
michaelknz <mihail2003rar@mail.ru>
2020-06-02 00:06:42 +0300

    Add player and movement

---
+++ Assets/scripts/playerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    private float speed;
+
+    void Start()
+    {
+        speed = 5.0f;
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime * (-1), 0, 0);
+        }
+    }
+}
