5c8e325fae94da1cb9a111bea558a4a09de7dc22
michaelknz <mihail2003rar@mail.ru>
2020-06-03 00:17:10 +0300

    Add jump without controlling

--- Assets/scripts/playerScript.cs
+++ Assets/scripts/playerScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,26 +8,47 @@ public class playerScript : MonoBehaviour
     public float speed;
     public float jump_force;
     private Rigidbody2D rb;
+    public float speed_del;
+    private bool is_jump;
+    public float max_speed;
+    public float radius_del;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        rb.velocity = new Vector2(0,0);
+        is_jump = false;
     }
 
     
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && (!is_jump))
         {
-            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            rb.velocity += new Vector2(Mathf.Min(speed,max_speed-rb.velocity.x), 0);
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && (!is_jump))
         {
-            transform.position += new Vector3(speed * Time.deltaTime * (-1), 0, 0);
+            rb.velocity += new Vector2(Mathf.Max(-speed,-max_speed-rb.velocity.x), 0);
         }
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && (!is_jump))
         {
             rb.AddForce(new Vector2(0, jump_force));
+            is_jump = true;
         }
+        if (Check_Gnd())
+        {
+            is_jump = false;
+        }
+    }
+
+    
+
+    bool Check_Gnd()
+    {
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        int layer = (1 << 8);
+        return Physics2D.OverlapCircle(transform.position-new Vector3(0.0f,1.21f,0f), 0.45f, layer);
     }
 }
