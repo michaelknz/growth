ef558f451355fefac5b2f2fd56f2946f55340819
michaelknz <mihail2003rar@mail.ru>
2020-06-04 03:23:26 +0300

    Add sit down, double jump, increse speed

--- Assets/scripts/animations_for_player.cs
+++ Assets/scripts/animations_for_player.cs
@@ -5,24 +5,29 @@ using UnityEngine;
 public class animations_for_player : MonoBehaviour
 {
     Animator anim;
+    private bool is_jump;
     void Start()
     {
         anim = GetComponent<Animator>();
+        is_jump = false;
     }
     void Update()
-    {
+    { 
         float move = Input.GetAxis("Horizontal");
         anim.SetFloat("Speed", move);
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D) && !is_jump)
         {
             anim.SetTrigger("right");
         }
-        else if (Input.GetKeyDown(KeyCode.A))
+        else if (Input.GetKeyDown(KeyCode.A) && !is_jump)
         {
             anim.SetTrigger("left");
         }
     }
-    
 
+    public void Set_Jump(bool val)
+    {
+        is_jump = val;
+    }
 }
 
--- Assets/scripts/playerScript.cs
+++ Assets/scripts/playerScript.cs
@@ -11,21 +11,38 @@ public class playerScript : MonoBehaviour
     private Rigidbody2D rb;
     public float speed_del;
     private bool is_jump;
+    private bool is_double_jump;
     public float max_speed;
+    private bool first_time;
     private Vector3 dir;
+    private animations_for_player anim;
+    public float sit_down_del;
+    private bool s_flag;
+
+    void Awake()
+    {
+        is_jump = false;
+        is_double_jump = false;
+        first_time = true;
+    }
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
         dir = new Vector3(0, 0, 0);
-        is_jump = false;
+        anim = GetComponent<animations_for_player>();
+        s_flag = false;
     }
 
     
     void Update()
     {
         is_jump = !Check_Gnd();
+        if (!first_time)
+        {
+            anim.Set_Jump(is_jump);
+        }
         if (Input.GetKey(KeyCode.D) && (!is_jump))
         {
             dir += new Vector3(Mathf.Min(speed,max_speed-dir.x), 0, 0);
@@ -34,13 +51,25 @@ public class playerScript : MonoBehaviour
         {
             dir += new Vector3(Mathf.Max(-speed,-max_speed-dir.x), 0, 0);
         }
-        if (Input.GetKeyDown(KeyCode.W) && (!is_jump))
+        if (Input.GetKeyDown(KeyCode.W) && (!is_double_jump))
         {
             rb.AddForce(new Vector2(0, jump_force));
-            is_jump = true;
+            if (is_jump)
+            {
+                is_double_jump = true;
+            }
         }
-
-        Debug.Log(is_jump);
+        if (Input.GetKey(KeyCode.S) && !s_flag)
+        {
+            BoxCollider2D collider = GetComponent<BoxCollider2D>();
+            collider.size -= new Vector2(0.0f, sit_down_del);
+            collider.offset -= new Vector2(0, sit_down_del / 2);
+            s_flag = true;
+        }
+        if (!(is_jump))
+        {
+            is_double_jump = false;
+        }
 
         transform.position += dir * Time.deltaTime;
         if (!is_jump)
@@ -54,17 +83,25 @@ public class playerScript : MonoBehaviour
                 dir -= new Vector3(Mathf.Min(speed_del, dir.x), 0, 0);
             }
         }
-        Debug.Log(dir.x);
+        if (is_jump == false)
+        {
+            first_time = false;
+        }
+        if (!Input.GetKey(KeyCode.S) && s_flag)
+        {
+            BoxCollider2D collider = GetComponent<BoxCollider2D>();
+            collider.size += new Vector2(0.0f, sit_down_del);
+            collider.offset += new Vector2(0, sit_down_del / 2);
+            s_flag = false;
+        }
     }
 
     
 
-    bool Check_Gnd()
+    public bool Check_Gnd()
     {
         BoxCollider2D collider = GetComponent<BoxCollider2D>();
         int layer = (1 << 8);
-        //return Physics2D.OverlapCircle(transform.position-new Vector3(0.0f,0.65f,0f), 0.22f, layer);
-        Debug.Log(Physics2D.OverlapBox(collider.bounds.center, collider.bounds.size, 0.0f, layer));
         return Physics2D.OverlapBox(collider.bounds.center, collider.bounds.size, 0.0f, layer);
     }
 }
